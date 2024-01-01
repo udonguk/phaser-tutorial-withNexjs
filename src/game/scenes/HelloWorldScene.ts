037d8d5fc9f50820b2586e24ec156643a31e15ff
류동욱 <udonguk@widebrain.kr>
2024-01-01 23:41:23 +0900

    + 캐릭터 추가

--- src/game/scenes/HelloWorldScene.ts
+++ src/game/scenes/HelloWorldScene.ts
@@ -6,10 +6,33 @@ export default class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('background', '/game/house/bg_repeat_340x640.png')
+
+        this.load.atlas(
+            'rocket-mouse',
+            'game/characters/rocket-mouse.png',
+            'game/characters/rocket-mouse.json'
+        )
     }
 
+
+
     create() {
-        this.add.image(0, 0, 'background')
-            .setOrigin(0, 0)
+        const width = this.scale.width
+        const height = this.scale.height
+
+        this.add.tileSprite(0, 0, width, height, 'background')
+            .setOrigin(0)
+
+        this.add.sprite(
+            width * 0.5,
+            height * 0.5,
+            'rocket-mouse',
+            'fly/roketmouse-fly01.png'
+        ).setScale(0.5, 0.5)
+
+        // this.anims.generateFrameNames('rocket-mouse-run', {
+        //     start: 1, end: 4, zeroPad: 2,
+        //     prefix: 'run', suffix: '.png'
+        // });
     }
 }
